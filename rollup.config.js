8fd68ae67d8f3a616018a285e0a5c55f32434f56
Gustavo Guichard <gustavoguichard@gmail.com>
2019-06-21 17:01:19 -0300

    Update example CSS and add postcss process to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import resolve from 'rollup-plugin-node-resolve'
 import external from 'rollup-plugin-peer-deps-external'
 import babel from 'rollup-plugin-babel'
 import json from 'rollup-plugin-json'
+import postcss from 'rollup-plugin-postcss'
 import pkg from './package.json'
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
@@ -15,6 +16,9 @@ const config = {
   plugins: [
     // Allows node_modules resolution
     resolve({ extensions }),
+
+    // Allow CSS imports
+    postcss(),
     // Allow bundling cjs modules. Rollup doesn't understand cjs
     commonjs({
       include: /node_modules/,
