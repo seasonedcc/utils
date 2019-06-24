4b7109ef8067be4d907e4bd04ffc20b9b65246dd
Gustavo Guichard <gustavoguichard@gmail.com>
2019-06-24 15:23:49 -0300

    Change scaffold.js

--- example/src/Example.js
+++ example/src/Example.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { LIBRARY_NAME } from 'new-react-lib'
 
-const Example = () => <div>{foo()}</div>
+const Example = () => (
+  <div>
+    <LIBRARY_NAME />
+  </div>
+)
 
 export default Example
