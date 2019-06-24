eec4dde4b0beb4f46f86011e3e1676d2e1e4feb1
Gustavo Guichard <gustavoguichard@gmail.com>
2019-06-24 20:49:58 -0300

    Add Syntax highlighter to sample

--- example/src/Example.js
+++ example/src/Example.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism'
+import { Card, CardHeader, CardContent } from '@material-ui/core'
+
 import { LIBRARY_NAME } from 'new-react-lib'
 
 const Example = () => (
-  <div>
-    <LIBRARY_NAME />
-  </div>
+  <Card elevation={5} className="rating">
+    <CardHeader title="My sample" />
+    <CardContent>
+      <LIBRARY_NAME />
+      <SyntaxHighlighter language="javascript" style={prism}>
+        {`
+import { LIBRARY_NAME } from 'seasoned-evaluator'
+
+// MyComponent
+<LIBRARY_NAME />
+        `}
+      </SyntaxHighlighter>
+    </CardContent>
+  </Card>
 )
 
 export default Example
