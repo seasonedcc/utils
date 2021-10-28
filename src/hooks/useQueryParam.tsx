b332a0a94f9a9cce1620dbf605222c127c5dc08c
Guilherme Guerra <guilherme.ga@gmail.com>
2021-10-28 15:56:20 -0300

    Adds error handling for useQueryParam

--- src/hooks/useQueryParam.tsx
+++ src/hooks/useQueryParam.tsx
@@ -1,6 +1,9 @@
 import isArray from 'lodash/isArray'
+import isNull from 'lodash/isNull'
+import isUndefined from 'lodash/isUndefined'
 
 export function useQueryParam(param: string | Array<string>) {
+  if (isNull(param) || isUndefined(param)) throw new Error('Missing param for useQueryParam')
   const params = new URLSearchParams(window.location.search)
   return isArray(param) ? param.reduce((pv, current) => ({...pv, [current]: params.get(current)}), {}) : params.get(param)
 }
